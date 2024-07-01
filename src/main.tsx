86c256c6c364943d5d8d9b5a3f9455c52b9875db
milvalen <artemyerkov@gmail.com>
2024-07-01 17:19:58 +0300

    build

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,12 @@ import { AddJob } from './jobs';
 import './app/index.pcss';
 import './assets/styles/reset.scss';
 import './assets/styles/global.scss';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="*" element={<AddJob />} />
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
