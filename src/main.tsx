104f09b14b88726d68c8c7e8bd506541e1ca14c6
milvalen <artemyerkov@gmail.com>
2024-07-01 17:01:17 +0300

    all routes addJob

--- src/main.tsx
+++ src/main.tsx
@@ -3,5 +3,12 @@ import { AddJob } from './jobs';
 import './app/index.pcss';
 import './assets/styles/reset.scss';
 import './assets/styles/global.scss';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(<AddJob />);
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="*" element={<AddJob />} />
+    </Routes>
+  </BrowserRouter>
+);
